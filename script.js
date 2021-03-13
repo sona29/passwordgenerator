a3e84a4a277bc87afc19dddd96f77c3663c07994
Sona Shrestha <sona.shrestha004@gmail.com>
2021-03-13 18:14:02 +1100

    adding function to generate password

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var numbers ="0123456789";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
+//Function for generating password
+var generatePassword = function() {
+  var passwordLength = window.prompt("Password Length");
+}
 
 
