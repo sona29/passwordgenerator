6111710baeed381b8eccc3e083575e0d85ce99b3
Sona Shrestha <sona.shrestha004@gmail.com>
2021-03-13 18:11:43 +1100

    declaring variables

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var specialChars = "!@#$%^&*(){}[]=<>/,.|~?";
+var numbers ="0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
