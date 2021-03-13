b9afddccf7a17a21c830611f622af4e819b8e2ac
Sona Shrestha <sona.shrestha004@gmail.com>
2021-03-13 18:26:32 +1100

    if only special chars are chosen

--- script.js
+++ script.js
@@ -3,39 +3,54 @@ var numbers ="0123456789";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-//Function for generating password
+//Function to generate password
 var generatePassword = function() {
+  var specialChars = "!@#$%^&*(){}[]=<>/,.|~?";
+  retVal = "";
+
   var passwordLength = window.prompt("Password Length");
-  
-  //if password length is not entered
+  //If password length is not chosen
   if(!passwordLength){
     window.alert("Password must have specific length");
     return;
   }
-
   //if password length is less than 8
   if(passwordLength<8){
     window.alert("Password cannot be less than 8 characters");
     return;
   }
 
-    //if password length is greater than 128
+  //if password length is greate than 128
    if(passwordLength>128)
     {
       window.alert("Password cannot be greater than 128 characters");
     return;
     }
- 
-    //Confirming other conditions
+
     var specialCharsConfirm = window.confirm("Do you want special chars?");
     var numbersConfirm = window.confirm("Do you want numbers?");
     var lowerCaseConfirm = window.confirm("Do you want lower case alphabets?");
     var upperCaseConfirm = window.confirm("Do you want upper case alphabets?");
+    
+    //If no other options for password is chosen
+    if(!specialCharsConfirm && !numbersConfirm && !lowerCaseConfirm && !upperCaseConfirm)
+    {
+      window.alert("Must choose atleast one condition");
+    return;
+    }
+    else if(specialCharsConfirm && !numbersConfirm && !lowerCaseConfirm && !upperCaseConfirm){
+      console.log(specialChars.length);
 
-    console.log(specialCharsConfirm);
-    console.log(numbersConfirm);
-    console.log(lowerCaseConfirm);
-    console.log(upperCaseConfirm);
+      for (var i = 0, n = specialChars.length; i < passwordLength; ++i) {
+        retVal += specialChars.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(retVal);
+      
+    }
+
+   
 }
+
+generatePassword();
 
 
